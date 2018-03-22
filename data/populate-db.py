87f6d1eae1f6bf9b658a506ea13a626f723d2420
Will Neville <william@neville.com>
2018-03-21 23:14:12 -0400

    updates to data files to include subtype (e.g. firearm for weapon, or chest for armor)

--- data/populate-db.py
+++ data/populate-db.py
@@ -1,14 +1,15 @@
 from tinydb import TinyDB
 from os import listdir
 
-def map_item_to_json(item_type, item_details):
+def map_item_to_json(item_type, item_subtype, item_details):
     if item_type == 'armor':
         return {
             'name': item_details[0].strip(),
             'set': item_details[1].strip(),
             'plus': item_details[2].strip(),
             'chalice': item_details[3].strip(),
-            'fashion': item_details[4].strip()
+            'fashion': item_details[4].strip(),
+            'subtype': item_subtype
         }
     if item_type == 'weapon':
         return {
@@ -18,17 +19,19 @@ def map_item_to_json(item_type, item_details):
             'blt_req': item_details[3].strip(),
             'arc_req': item_details[4].strip(),
             'plus': item_details[5].strip(),
-            'chalice': item_details[6].strip()
+            'chalice': item_details[6].strip(),
+            'subtype': item_subtype
         }
 
-def populate_table(db, name):
-    table = db.table(name)
-    with open(name + '.txt') as f:
+def populate_table(db, item_file):
+    table = db.table(item_file[:-4])
+    with open(item_file) as f:
         item_type = f.readline().strip()
+        item_subtype = f.readline().strip() #e.g. head, hand, firearm, etc.
         for row in f:
             if row[0] != '#':
                 item_details_list = row.split(',')
-                item = map_item_to_json(item_type, item_details_list)
+                item = map_item_to_json(item_type, item_subtype, item_details_list)
                 table.insert(item)
 
 def main():
@@ -41,7 +44,7 @@ def main():
             all_item_data_files.append(data_file)
     
     for item_file in all_item_data_files:
-        populate_table(db, item_file[:-4])
+        populate_table(db, item_file)
 
 
 if __name__ == '__main__':
