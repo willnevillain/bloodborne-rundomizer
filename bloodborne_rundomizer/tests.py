c4e9537308650d8e867cb47f843f9c6909f3bde5
Will Neville <william@neville.com>
2018-04-25 11:15:43 -0400

    refactoring and expanding test suite

--- bloodborne_rundomizer/tests.py
+++ bloodborne_rundomizer/tests.py
@@ -4,57 +4,46 @@ from models import Weapon, Armor
 
 class TestFilters(unittest.TestCase):
 
+    def setUp(self):
+        self.armor = rundomizer.populate_armor()
+        self.weapons = rundomizer.populate_weapons()
+
     def test_remove_plus_armor(self):
-        all_armor = rundomizer.populate_armor()
-        filtered = rundomizer.remove_plus_items(all_armor)
+        filtered = rundomizer.remove_plus_items(self.armor)
         for piece in filtered:
             self.assertTrue(piece.location != 'NG+')
-        self.assertTrue(len(filtered) < len(all_armor))
+        self.assertTrue(len(filtered) < len(self.armor))
             
-
     def test_remove_plus_weapons(self):
-        all_weapons = rundomizer.populate_weapons()
-        filtered = rundomizer.remove_plus_items(all_weapons)
+        filtered = rundomizer.remove_plus_items(self.weapons)
         for weapon in filtered:
             self.assertTrue(weapon.location != 'NG+')
-        self.assertTrue(len(filtered) < len(all_weapons))
-            
+        self.assertTrue(len(filtered) < len(self.weapons))            
 
     def test_remove_chalice_armor(self):
-        all_armor = rundomizer.populate_armor()
-        filtered = rundomizer.remove_chalice_items(all_armor)
+        filtered = rundomizer.remove_chalice_items(self.armor)
         for piece in filtered:
             self.assertTrue(piece.location != 'Chalice')
-        self.assertTrue(len(filtered) < len(all_armor))
-
+        self.assertTrue(len(filtered) < len(self.armor))
 
     def test_remove_chalice_weapons(self):
-        all_weapons = rundomizer.populate_weapons()
-        filtered = rundomizer.remove_chalice_items(all_weapons)
+        filtered = rundomizer.remove_chalice_items(self.weapons)
         for weapon in filtered:
             self.assertTrue(weapon.location != 'Chalice')
-        self.assertTrue(len(filtered) < len(all_weapons))
-
+        self.assertTrue(len(filtered) < len(self.weapons))
 
     def test_remove_big_guns(self):
-        all_weapons = rundomizer.populate_weapons()
-        filtered = rundomizer.remove_big_guns(all_weapons)
+        filtered = rundomizer.remove_big_guns(self.weapons)
         for weapon in filtered:
             if weapon.slot == 'Firearm':
                 self.assertTrue(weapon.requirements['str'] < 27)
-        self.assertTrue(len(filtered) < len(all_weapons))
-
+        self.assertTrue(len(filtered) < len(self.weapons))
     
     def test_remove_fashionable_armor(self):
-        all_armor = rundomizer.populate_armor()
-        filtered = rundomizer.remove_fashionable_items(all_armor)
+        filtered = rundomizer.remove_fashionable_items(self.armor)
         for piece in filtered:
             self.assertTrue(piece.fashionable == 'False')
-        self.assertTrue(len(filtered) < len(all_armor))
-
-
-    
-            
+        self.assertTrue(len(filtered) < len(self.armor))
 
 
 if __name__ == '__main__':
