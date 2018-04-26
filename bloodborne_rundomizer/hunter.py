c0f9c6f518770549ca201a8c4df96dd54ece9f1f
Will Neville <william@neville.com>
2018-04-25 22:12:57 -0400

    Hunter class now holds and displays req_stat info

--- bloodborne_rundomizer/hunter.py
+++ bloodborne_rundomizer/hunter.py
@@ -3,17 +3,25 @@ from models import Weapon, Armor
 class Hunter:
     
     def __init__(self, equipment):
-        self.equipment = []
-        self.equipped = {}
-        self.req_stats = {}
-
-        self.bind_equipment(equipment)
+        self.equipped = self.bind_equipment(equipment)
+        self.req_stats = self.calculate_stats()
 
     def bind_equipment(self, equipment):
+        equipped = {}
         for piece in equipment:
-            self.equipped[piece.slot] = piece
-            
+            equipped[piece.slot] = piece
+        return equipped
+
+    def calculate_stats(self):
+        req_stats = {}
+        for piece in self.equipped.values():
+            if isinstance(piece, Weapon):
+                for stat in piece.requirements:
+                    if (stat not in req_stats) or (req_stats[stat] < piece.requirements[stat]):
+                        req_stats[stat] = piece.requirements[stat]
+        return req_stats
                 
     def display_info(self):
         for slot, piece in self.equipped.items():
             print(slot + ': ' + str(piece))
+        print('Stat requirements: ' + str(self.req_stats))
